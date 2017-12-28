12436659b4ec53babe2a702870acf7aed64974b9
antonysan <tonik32@gmail.com>
2017-12-28 12:50:33 +0300

    Обработка клика по modal-button-close

--- js/javascript.js
+++ js/javascript.js
@@ -1,8 +1,17 @@
 var userEnter = document.querySelector(".user-item-link");
 var userModal = document.querySelector(".user-modal");
+var buttonClose = document.querySelector(".user-modal-close");
+var modalFocus = document.querySelector(".user-input-name");
 
 userEnter.addEventListener("click", function(evt){
   evt.preventDefault();
-  console.log('Вход');
+  console.log('Кнопка Вход');
   userModal.classList.add("modal-show");
+  modalFocus.focus();
+});
+
+buttonClose.addEventListener("click", function(evt){
+  evt.preventDefault();
+  console.log('Кнопка выход из модального окна');
+  userModal.classList.remove("modal-show");
 });
