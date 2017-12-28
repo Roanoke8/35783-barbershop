3c3492934ee1cd52b3d5d4b58e45cbe4e59d55a5
antonysan <tonik32@gmail.com>
2017-12-28 16:15:21 +0300

    Модальное окно с картой

--- js/javascript.js
+++ js/javascript.js
@@ -2,6 +2,9 @@ var userEnter = document.querySelector(".user-item-link");
 var userModal = document.querySelector(".user-modal");
 var buttonClose = document.querySelector(".user-modal-close");
 var modalFocus = document.querySelector(".user-input-name");
+var callMaps = document.querySelector(".info-callmaps-button");
+var mapsModal = document.querySelector(".maps-modal");
+var mapsClose = document.querySelector(".maps-button-close");
 
 userEnter.addEventListener("click", function(evt){
   evt.preventDefault();
@@ -14,4 +17,13 @@ buttonClose.addEventListener("click", function(evt){
   evt.preventDefault();
   console.log('Кнопка выход из модального окна');
   userModal.classList.remove("modal-show");
+});
+callMaps.addEventListener("click", function(evt){
+  evt.preventDefault();
+  console.log('Клик по кнопке Как проехать');
+  mapsModal.classList.add("maps-modal-show");
+});
+mapsClose.addEventListener("click", function(evt){
+  evt.preventDefault();
+  mapsModal.classList.remove("maps-modal-show");
 });
